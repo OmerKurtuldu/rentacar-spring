9722ba782d53da18397f5d3d92eb074581280495
DESKTOP-MK6RBJN\LENOVO <omerkurtuldu11@gmail.com>
2024-03-29 12:01:05 +0300

    Add new feature

--- src/main/java/com/turkcell/rentacar/dataAccess/abstracts/AdditionalRepository.java
+++ src/main/java/com/turkcell/rentacar/dataAccess/abstracts/AdditionalRepository.java
@@ -3,5 +3,5 @@ package com.turkcell.rentacar.dataAccess.abstracts;
 import com.turkcell.rentacar.entities.concretes.AdditionalFeature;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdditionalRepository  extends JpaRepository<Integer,AdditionalFeature> {
+public interface AdditionalRepository  extends JpaRepository<AdditionalFeature,Integer> {
 }
